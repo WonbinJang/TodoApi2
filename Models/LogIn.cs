09269631cdd9ca9fbf8b4d3c21da02cecb48b748
WonbinJang <155414548+WonbinJang@users.noreply.github.com>
2024-01-22 14:40:32 +0900

    jwt hash password x

---
+++ Models/LogIn.cs
@@ -0,0 +1,8 @@
+namespace TodoApi.Models
+{
+public class User{
+        public string? userName {get;set;}
+
+        public string? Password {get;set;}
+    }
+    }
---
+++ Models/TodoStatus.cs
@@ -0,0 +1,8 @@
+namespace TodoApi.Models
+{
+    public class TodoStatus
+    {
+        public int Id { get; set; }
+        public int Status { get; set; }
+    }
+}
